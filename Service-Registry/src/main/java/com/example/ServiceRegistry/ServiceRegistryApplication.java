ba291588ec182b1c82f3f9efa0a68005c69f21d3
ELKADDIRI Mohamed <mohamedelkaddiri@gmail.com>
2023-12-25 12:22:40 +0100

    First commit

--- Service-Registry/src/main/java/com/example/ServiceRegistry/ServiceRegistryApplication.java
+++ Service-Registry/src/main/java/com/example/ServiceRegistry/ServiceRegistryApplication.java
@@ -3,9 +3,9 @@ package com.example.ServiceRegistry;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-@EnableEurekaServer
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
